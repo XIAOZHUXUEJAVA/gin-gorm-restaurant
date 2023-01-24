e704e81082dd0777e5237d0d5641d16ef456e5f1
XIAOZHUXUEJAVA <2393751426@qq.com>
2023-01-24 17:54:49 +0800

    commit frontend

---
+++ frontend/restaurant-ordering-system/frontend/src/store/index.js
@@ -0,0 +1,38 @@
+import {createStore} from "vuex"
+import axios from "axios"
+
+const store = createStore({
+    state() {
+        return {
+            allFoods: [],
+            user: undefined,
+            admin: undefined,
+        }
+    },
+    mutations: {
+        setFoodsData(state, payload){
+            state.allFoods = payload;
+        },
+        setUser(state, payload){
+            state.user = payload;
+        },
+        setAdmin(state, payload){
+            state.admin = payload;
+        }
+    },
+    actions: {
+        async getFoodsData(context){
+            await axios.get('/foods')
+            .then(function (response) {
+                console.log(response.data);
+                console.log(response.data.data);
+                context.commit("setFoodsData", response.data.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        },
+    }
+})
+
+export default store;
